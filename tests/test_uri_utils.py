e5386d013a0dccb6eb11223d90dfb2656e1eb89e
Russel Torres <russtorres@gmail.com>
2021-07-20 11:54:38 -0700

    test: add custom scheme and smart_open tests

--- tests/test_uri_utils.py
+++ tests/test_uri_utils.py
@@ -26,9 +26,11 @@ import uri_handler.utils.uri_utils
          ("thing",),
          "file:///path/to/thing"),
         # uri with all components (except obsolete ;parameters)
-        ("myscheme://mynetloc/path/to?myqparam=null&myotherqparam=null#myfragment",
+        ("myscheme://mynetloc/path/to?"
+         "myqparam=null&myotherqparam=null#myfragment",
          ("thing",),
-         "myscheme://mynetloc/path/to/thing?myqparam=null&myotherqparam=null#myfragment")
+         "myscheme://mynetloc/path/to/thing?"
+         "myqparam=null&myotherqparam=null#myfragment")
     ])
 def test_join_uri(uri, stargs_to_join, expected_joined_uri):
     """test join uri for various uris"""
@@ -40,13 +42,18 @@ def test_join_uri(uri, stargs_to_join, expected_joined_uri):
 @pytest.mark.parametrize(
     "uri,expected_prefix", [
         # full uri with
-        ("myscheme://mynetloc/path/to/thing?myqparam=null&myotherqparam=null#myfragment",
-         "myscheme://mynetloc/path/to?myqparam=null&myotherqparam=null#myfragment"),
+        ("myscheme://mynetloc/path/to/thing?"
+         "myqparam=null&myotherqparam=null#myfragment",
+         "myscheme://mynetloc/path/to?"
+         "myqparam=null&myotherqparam=null#myfragment"),
         # uri with trailing / in path
-        ("myscheme://mynetloc/path/to/path/?myqparam=null&myotherqparam=null#myfragment",
-         "myscheme://mynetloc/path/to?myqparam=null&myotherqparam=null#myfragment"),
+        ("myscheme://mynetloc/path/to/path/?"
+         "myqparam=null&myotherqparam=null#myfragment",
+         "myscheme://mynetloc/path/to?"
+         "myqparam=null&myotherqparam=null#myfragment"),
         # uri at top level
-        ("myscheme://mynetloc/mything?myqparam=null&myotherqparam=null#myfragment",
+        ("myscheme://mynetloc/mything?"
+         "myqparam=null&myotherqparam=null#myfragment",
          "myscheme://mynetloc/?myqparam=null&myotherqparam=null#myfragment"),
         # uri of top level (acts like os.path.dirname)
         ("myscheme://mynetloc/?myqparam=null&myotherqparam=null#myfragment",
--- tests/test_uris.py
+++ tests/test_uris.py
@@ -1,8 +1,22 @@
+import os
 import pytest
 
+import marshmallow.fields
+
 import uri_handler.uri_functions
+import uri_handler.storage.custom_schemes
 
 from conftest import uribase_fixtures_to_test
+
+# test smart_open if installed or explicitly asked for
+TEST_SMART_OPEN = os.getenv("UH_TEST_SMART_OPEN")
+if TEST_SMART_OPEN is None:
+    try:
+        import smart_open  # noqa: F401
+        TEST_SMART_OPEN = True
+    except ImportError:
+        TEST_SMART_OPEN = False
+TEST_SMART_OPEN = marshmallow.fields.Bool().deserialize(TEST_SMART_OPEN)
 
 
 def test_tests():
@@ -29,6 +43,29 @@ def test_readwrite_uri(uri_base_fixture, request):
 
     r = uri_handler.uri_functions.uri_readbytes(
         test_uri)
+
+    assert r == test_b
+    assert r.decode("UTF-8") == test_str
+
+
+@pytest.mark.skipif(not TEST_SMART_OPEN, reason=(
+    "smart_open is not installed or explicitly not being tested"))
+@pytest.mark.parametrize(
+    "uri_base_fixture", uribase_fixtures_to_test)
+def test_smartopen_uri(uri_base_fixture, request):
+    uri = request.getfixturevalue(uri_base_fixture)
+
+    test_str = "test_so"
+    test_b = test_str.encode("UTF-8")
+
+    test_uri = uri_handler.uri_functions.uri_join(
+        uri, "test_so_data")
+
+    with uri_handler.uri_functions.uri_smart_open(test_uri, "wb") as sof:
+        sof.write(test_b)
+
+    with uri_handler.uri_functions.uri_smart_open(test_uri, "rb") as sof:
+        r = sof.read()
 
     assert r == test_b
     assert r.decode("UTF-8") == test_str
