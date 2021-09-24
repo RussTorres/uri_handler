237fa929835b4a1efe9e9747882d879d703a54a0
Russel Torres <russtorres@gmail.com>
2021-09-24 09:45:47 -0700

    filestorage: allow creating parents for smart_open

--- uri_handler/storage/filestorage.py
+++ uri_handler/storage/filestorage.py
@@ -11,6 +11,16 @@ from uri_handler.utils._compat import (
     urllib,
     pathlib)
 
+import uri_handler.storage.custom_schemes
+
+_file_create_modes = {
+    "w", "a", "x",
+    "w+", "a+", "x+",
+    "w+b", "a+b", "x+b",
+    "w+t", "a+t", "x+t",
+    "wb", "ab", "xb",
+    "wt", "at", "xt"}
+
 
 def file_readbytes(fn):
     with open(fn, 'rb') as f:
@@ -67,6 +77,55 @@ def uri_to_filename(uri):
     return urllib.parse.unquote(urllib.parse.urlparse(str(uri)).path)
 
 
+def get_local_file_uri(uri):
+    """get local file uri from custom scheme or query params"""
+    p = urllib.parse.urlparse(uri)
+
+    customschemeparams = uri_handler.storage.custom_schemes.custom_schemes.get(
+        p.scheme, {}).get("scheme_params", {})
+
+    scheme_path = customschemeparams.get("path_prefix", "/")
+
+    new_path = os.path.join(scheme_path, p.path.lstrip("/"))
+
+    file_uri = urllib.parse.urlunparse(
+        urllib.parse.ParseResult(
+            "file",
+            "",
+            new_path,
+            "",
+            "",
+            "",
+        ))
+    return file_uri
+
+
+def conform_uri_to_scheme(uri, target_scheme_like=None, target_scheme=None):
+    """conform file uri to a custom scheme"""
+    if target_scheme_like is None:
+        if target_scheme is None:
+            return uri
+    else:
+        target_scheme = urllib.parse.urlparse(target_scheme_like).scheme
+
+    local_prefix = uri_handler.storage.custom_schemes.custom_schemes.get(
+        target_scheme, {}).get("scheme_params", {}).get("path_prefix", "")
+    local_path = urllib.parse.urlparse(uri).path
+
+    new_path = (local_path[len(local_prefix):]
+                if local_path.startswith(local_prefix)
+                else local_path)
+    new_uri = urllib.parse.urlunparse(urllib.parse.ParseResult(
+        target_scheme,
+        "",
+        "/" + new_path.lstrip("/"),
+        "",
+        "",
+        ""
+    ))
+    return new_uri
+
+
 def filename_to_uri(fn):
     return pathlib.Path(fn).as_uri()
 
@@ -84,11 +143,17 @@ def listfiles_from_dir(d, full=True):
             yield ifull
 
 
-def file_listuris_from_prefix(uri):
+def _canonical_file_listuris_from_prefix(uri):
     d = uri_to_filename(uri)
     fpaths = listfiles_from_dir(d, full=True)
     for fpath in fpaths:
         yield filename_to_uri(fpath)
+
+
+def file_listuris_from_prefix(uri):
+    local_prefix = get_local_file_uri(uri)
+    for fpath in _canonical_file_listuris_from_prefix(local_prefix):
+        yield conform_uri_to_scheme(fpath, uri)
 
 
 def get_deep_directory_exists(d):
@@ -103,7 +168,7 @@ def get_deep_directory_exists(d):
             return get_deep_directory_exists(newd)
 
 
-def file_validate_prefix(uri, check_writable=True, **kwargs):
+def _canonical_file_validate_prefix(uri, check_writable=True, **kwargs):
     d = uri_to_filename(uri)
 
     # get deepest existing directory
@@ -114,24 +179,54 @@ def file_validate_prefix(uri, check_writable=True, **kwargs):
     return True
 
 
+def file_validate_prefix(uri, *args, **kwargs):
+    local_uri = get_local_file_uri(uri)
+    return _canonical_file_validate_prefix(local_uri)
+
+
+def file_uri_writebytes(b, uri, *args, **kwargs):
+    local_uri = get_local_file_uri(uri)
+    return file_writebytes(
+        b, uri_to_filename(local_uri),
+        *args, **kwargs)
+
+
+def file_uri_readbytes(uri):
+    local_uri = get_local_file_uri(uri)
+    return file_readbytes(uri_to_filename(local_uri))
+
+
 class FileUriHandler(BaseUriHandler):
     atomic = True
     make_outdir = True
     overwrite = True
 
     def save_bytes(self, b, uri):
-        return file_writebytes(
-            b, uri_to_filename(uri),
+        return file_uri_writebytes(
+            b, uri,
             self.atomic, self.make_outdir, self.overwrite)
 
     def write_bytes(self, uri, b):
         return self.save_bytes(b, uri)
 
     def read_bytes(self, uri):
-        return file_readbytes(uri_to_filename(uri))
+        return file_uri_readbytes(uri)
 
     def list_uris_prefix(self, uri_prefix):
         return file_listuris_from_prefix(uri_prefix)
 
     def validate_prefix(self, uri_prefix, **kwargs):
         return file_validate_prefix(uri_prefix)
+
+    # FIXME currently python 3 specific kw-only arg
+    def _smart_open_uri(self, uri, *args, parents=None, **kwargs):
+        local_file_uri = get_local_file_uri(uri)
+        parents = parents or self.make_outdir
+        try:
+            mode = args[0]
+        except IndexError:
+            mode = kwargs.get("mode")
+        if parents and (mode in _file_create_modes):
+            makedirs_safe(os.path.dirname(uri_to_filename(local_file_uri)))
+
+        return super()._smart_open_uri(local_file_uri, *args, **kwargs)
